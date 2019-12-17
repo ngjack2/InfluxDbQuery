740a914bdd17653c014581c76572638d101dead6
ngjack2 <ngjack2@gmail.com>
2019-12-17 23:33:17 +0800

    1. Script to run MDA by days or month.

---
+++ run_mda_by_month.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import pdb
+import time
+import ctypes
+import xlwt
+import math
+import json
+import calendar
+import main
+
+# format time for one day query
+queryEnd = 'T16:00:00Z'
+queryStart = 'T15:59:00Z';
+
+cal= calendar.Calendar()
+x = [x for x in cal.itermonthdates(2019, 12)];
+
+for i in range(len(x)):
+    strt = x[i].strftime("%Y-%m-%d") + queryEnd;
+    end = x[i + 1].strftime("%Y-%m-%d") + queryStart;
+    print(strt, end);
+    main.main(strt, end);
+    if i == (len(x) - 2):
+        break;
+
